52db586236b7c2e4e9b53b907a658ca719954f42
uutan <58uutan92@gmail.com>
2024-03-12 08:17:52 +0900

    add:作成者のroleによって取得、表示するステージを分けるように実装

--- src/services/supabaseStages.js
+++ src/services/supabaseStages.js
@@ -13,13 +13,41 @@ export const getStageById = async (stageId) => {
   return { result: Response.success, data: stage };
 }
 
-export const getStagesRange = async ({ start, end }) => {
-  let { data: stages, error } = await supabase.from('stages').select().range(start, end);
-  if (error) {
-    return { result: Response.error, data: error };
+export const getDefaultStagesRange = async ({ start, end }) => {
+  try {
+    const { data: userProfiles, error: profilesError } = await supabase.from('profiles').select('id').eq('role', '管理者');
+
+    if (profilesError) throw profilesError;
+
+    const profileIds = userProfiles.map(profile => profile.id);
+
+    const { data: stages, error: stagesError } = await supabase.from('stages').select().in('profile_id', profileIds).range(start, end);
+
+    if (stagesError) throw stagesError;
+
+    return { result: Response.success, data: stages };
+  } catch (error) {
+    return { result: Response.error, data: error.message };
   }
-  return { result: Response.success, data: stages };
-}
+};
+
+export const getUsersStagesRange = async ({ start, end }) => {
+  try {
+    const { data: userProfiles, error: profilesError } = await supabase.from('profiles').select('id').eq('role', '一般');
+
+    if (profilesError) throw profilesError;
+
+    const profileIds = userProfiles.map(profile => profile.id);
+
+    const { data: stages, error: stagesError } = await supabase.from('stages').select().in('profile_id', profileIds).range(start, end);
+
+    if (stagesError) throw stagesError;
+
+    return { result: Response.success, data: stages };
+  } catch (error) {
+    return { result: Response.error, data: error.message };
+  }
+};
 
 export const getStagesRangeSortByDesc = async ({ start, end }) => {
   // NOTE : 追加順 = 制作日時が古い順になるはずなので、idでソートしています。
@@ -38,6 +66,40 @@ export const getStagesCount = async () => {
   }
   return { result: Response.success, count: count };
 }
+
+export const getStagesCountByAdmins = async () => {
+  try {
+    const { data: adminProfiles, error: profilesError } = await supabase.from('profiles').select('id').eq('role', '管理者');
+
+    if (profilesError) throw profilesError;
+
+    const profileIds = adminProfiles.map(profile => profile.id);
+    const { count, error: countError } = await supabase.from('stages').select('id',{ count: 'exact' }).in('profile_id', profileIds);
+
+    if (countError) throw countError;
+
+    return { result: Response.success, count };
+  } catch (error) {
+    return { result: Response.error, data: error.message };
+  }
+};
+
+export const getStagesCountByUsers = async () => {
+  try {
+    const { data: userProfiles, error: profilesError } = await supabase.from('profiles').select('id').eq('role', '一般');
+
+    if (profilesError) throw profilesError;
+
+    const profileIds = userProfiles.map(profile => profile.id);
+    const { count, error: countError } = await supabase.from('stages').select('id',{ count: 'exact' }).in('profile_id', profileIds);
+
+    if (countError) throw countError;
+
+    return { result: Response.success, count };
+  } catch (error) {
+    return { result: Response.error, data: error.message };
+  }
+};
 
 export const getStagesCountByUserId = async (userId) => {
   let { error, count } = await supabase.from('stages').select('id', { count: 'exact' }).eq('profile_id', userId);
