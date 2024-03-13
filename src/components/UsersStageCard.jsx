8790b4903ff0efa4f25dfda4739d318016c88c1b
uutan <58uutan92@gmail.com>
2024-03-13 21:22:49 +0900

    add:User'sタブの背景を追加

--- src/components/UsersStageCard.jsx
+++ src/components/UsersStageCard.jsx
@@ -3,6 +3,7 @@ import { STAGE_PER_PAGE } from 'utils/Paginate';
 import { Link } from 'react-router-dom';
 import { RoutePath } from 'utils/RouteSetting';
 import { getUsersStagesRange, getStagesCountByUsers } from 'services/supabaseStages';
+import "./stageCard.css"
 
 export const UsersStageCard = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -49,35 +50,34 @@ export const UsersStageCard = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div>
+    <article className='relative bg-yellow-300 p-4 rounded-b-md'>
       {/* 1つのステージを表示する要素 */}
-      <div className="grid grid-cols-2 gap-4">
+      <section className="grid grid-cols-2 gap-4">
         {stages.map(({ id, userName, title, image }) => (
-          <div key={id} className="relative m-2 p-6 border border-gray-300 shadow-lg rounded-md bg-white">
-            <div className="flex">
-               {/* ↓　【fix】このタグ調査、修正する。 */}
-              <div className="flex-grow"></div>
-              {/* ↓　【fix】src内のassetsから画像データを取得する方法が分からなかったため、現状はpublicから取得しています。 */}
-              <img src={`${process.env.PUBLIC_URL}/assets/imgs/defaultstage/${image}.png`} alt={title} />
+          <div key={id} className="relative flex m-2 p-6 border border-gray-300 shadow-lg rounded-md bg-white hover:translate-y-2 hover:shadow-none transition-all">
+            <div className='w-2/3'>
+              <img src={`/assets/imgs/defaultstage/${image}.png`} alt={title} />
             </div>
-            <p className="absolute top-16 left-4 text-3xl font-bold">User: {userName}</p>
-            <p className="absolute top-28 left-4 text-2xl font-bold">Title: {title}</p>
-            <Link
-              to={RoutePath.gamePlay.path(id)}
-              className="absolute bottom-14 left-16 bg-red-500 text-yellow-300 px-4 py-2 rounded shadow hover:bg-red-600 focus:outline-none focus:ring text-4xl">
-                ▶ Play
-            </Link>
+            <div className='w-1/3 flex flex-col justify-center items-center gap-10'>
+              <h4 className="text-2xl font-bold">User:{userName}</h4>
+              <h4 className="text-2xl font-bold">Title:{title}</h4>
+              <Link
+                to={RoutePath.gamePlay.path(id)}
+                className=" bg-red-500 text-yellow-300 px-4 py-2 rounded shadow hover:bg-yellow-300 focus:outline-none hover:text-red-500 focus:ring text-4xl transition-all play-link">
+                <span className='play-text'>Play</span>
+              </Link>
+            </div>
           </div>
         ))}
-      </div>
+      </section>
 
       {/* ページネーションのボタン部分 */}
-      <div className="flex justify-center mt-8">
+      <section className="flex justify-center mt-8">
         <button onClick={() => paginate(Math.max(1, currentPage - 1))} disabled={currentPage === 1}
-        className="bg-gray-300 text-black px-4 py-2 rounded-l hover:bg-gray-400 disabled:opacity-50">前のページへ</button>
+          className="bg-green-300 text-black px-4 py-2 rounded-l hover:bg-green-500 cursor-pointer border-lime-500 border transition-all">前のページへ</button>
         <button onClick={() => paginate(currentPage + 1)} disabled={currentPage >= Math.ceil(totalStages / STAGE_PER_PAGE)}
-        className="bg-gray-300 text-black px-4 py-2 rounded-r hover:bg-gray-400 disabled:opacity-50">次のページへ</button>
-      </div>
-    </div>
+            className="bg-green-300 text-black px-4 py-2 rounded-r hover:bg-green-500 cursor-pointer border-lime-500 border transition-all">次のページへ</button>
+      </section>
+    </article>
   );
 };
