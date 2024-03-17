e84eb1079f49343cffab7d34a97eb9f6d4d6f396
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-17 18:10:37 +0900

    list-noneが効かないのでdivに戻す

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import { RoutePath } from "utils/RouteSetting";
 export const Footer = () => {
   return (
     <footer className="sm:relative sm:flex sm:justify-between sm:items-center p-2.5 bg-green-400">
-      <ul className="text-end sm:text-start sm:flex sm:ml-10">
-        <li className="mx-1 inline-block sm:block">利用規約</li>
-        <li className="mx-1 inline-block sm:block"><Link to={RoutePath.privacy_policy.path}>{RoutePath.privacy_policy.name}</Link></li>
-        <li className="mx-1 inline-block sm:block">©Pythagora maker</li>
-      </ul>
+      <div className="text-end sm:text-start sm:flex sm:ml-10 list-none">
+        <div className="mx-1 inline-block sm:block">利用規約</div>
+        <div className="mx-1 inline-block sm:block"><Link to={RoutePath.privacy_policy.path}>{RoutePath.privacy_policy.name}</Link></div>
+        <div className="mx-1 inline-block sm:block">©Pythagora maker</div>
+      </div>
       <p className="text-base text-black mb-2 mx-4 hidden sm:block">
         ※本ゲームはPC専用となっております。
       </p>
